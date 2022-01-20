83b3349241d47bafd8f293b93dd9793077335407
Jami Dwyer <jamidwyer@yahoo.com>
2022-01-20 12:44:14 -0800

    add routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-import LandingPage from "../pages/LandingPage";
+import { Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import ResetPassword from "./ResetPassword";
+import { LandingPage } from "../pages/LandingPage";
 
 // TODO: move to hooks and functions
-const App = () => ( // eslint-disable-line
-  <div>
-    <LandingPage />
-  </div>
+// eslint-disable-next-line react/function-component-definition
+const App = () => (
+  <Routes>
+    <Route path="/" element={<LandingPage />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/signup" element={<SignUp />} />
+    <Route path="/reset-password" element={<ResetPassword />} />
+  </Routes>
 );
 
 export default App;
