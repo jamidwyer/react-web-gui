5d94eed2a2d3599f22e33b0389c5a34f6b7220ee
Jami Dwyer <jamidwyer@yahoo.com>
2022-02-12 15:55:59 -0800

    feat: add axe

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,23 @@
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
 import configureStore from "./store";
 
-render(
+require("dotenv").config({ path: ".env" });
+
+if (process.env.NODE_ENV !== "production") {
+  // eslint-disable-next-line
+  const axe = require("@axe-core/react");
+  axe(React, ReactDOM, 1000);
+}
+
+const config = process.env;
+ReactDOM.render(
   <Provider store={configureStore()}>
     <Router>
-      <App />
+      <App config={config} />
     </Router>
   </Provider>,
   document.getElementById("root"),
